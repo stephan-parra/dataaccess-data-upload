b54e9ffb57fdb2157e0f7b340256d8e5853f61c9
Stephan Parra <stephan.parra@pelicancorp.com>
2025-05-09 09:20:40 +0100

    Changed file size to return as bytes only

--- fileUpload.js
+++ fileUpload.js
@@ -103,13 +103,16 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function formatFileSize(bytes) {
-        if (bytes === 0) return '0 Bytes';
+        return bytes;
+
+        //Change the code so that it returns the size in bytes only without any units
+        /*if (bytes === 0) return '0 Bytes';
 
         const k = 1024;
         const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
         const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-        return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];*/
     }
 
     // Function to display response
